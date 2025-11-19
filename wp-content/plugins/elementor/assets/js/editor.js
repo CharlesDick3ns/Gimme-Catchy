cdb10f634094a3dfb0342b42dc7b2537f546dbe7
Gianluca Careccia <gianluca.careccia@gmail.com>
2025-11-19 01:42:24 +0100

    Aggiornamento sito: header e video corretti

--- wp-content/plugins/elementor/assets/js/editor.js
+++ wp-content/plugins/elementor/assets/js/editor.js
@@ -19053,7 +19053,7 @@ var URL = /*#__PURE__*/function (_BaseMultiple) {
         positionBase = elementorCommon.config.isRTL ? 'right' : 'left';
       var last, cache;
 
-      // Based on /wp-includes/js/tinymce/plugins/wplink/plugin.js.
+      // Based on ../wp-includes/js/tinymce/plugins/wplink/plugin.js.
       $mainInput.autocomplete({
         source: function source(request, response) {
           if (!_this.options.model.attributes.autocomplete) {
